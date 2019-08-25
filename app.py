a316061ceaef5ee62f865a951b59ca41df6071fd
Alex Philayvanh <aphilay@gmail.com>
2019-08-24 21:35:55 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from send_mail import send_mail
+
+# init app
+app = Flask(__name__)
+
+# set ENV to 'dev' or 'prod'
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/lexus'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://edbmmnputemkzw:492fdee6584e63fbd3149b1dce4e5e5a237fbb4a35141af79b323b2a4e23e5b7@ec2-23-23-182-18.compute-1.amazonaws.com:5432/d8amf9sbp8144r'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# set up db, use to query db
+db = SQLAlchemy(app)
+
+# create model, extends db.Model
+
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, dealer, rating, comments):
+        self.customer = customer
+        self.dealer = dealer
+        self.rating = rating
+        self.comments = comments
+
+
+# route decorator
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# submit function with POST method
+@app.route('/submit', methods=['POST'])
+def submit():
+    if request.method == 'POST':
+        customer = request.form['customer']
+        dealer = request.form['dealer']
+        rating = request.form['rating']
+        comments = request.form['comments']
+        # print(customer,dealer,rating,comments)
+        # validation
+        if customer == '' or dealer == '':
+            return render_template('index.html', message='Please enter required fields')
+        # check if customer does not exists, don't want same user submitting multiple times
+        if db.session.query(Feedback).filter(Feedback.customer == customer).count() == 0:
+            data = Feedback(customer, dealer, rating, comments)
+            db.session.add(data)
+            db.session.commit()
+            send_mail(customer, dealer, rating, comments)
+            return render_template('success.html')
+        return render_template('index.html', message='You have already submitted feedback')
+
+
+if __name__ == '__main__':
+    app.run()
+
+# TODO: UPLOAD PROJECT TO GITHUB
